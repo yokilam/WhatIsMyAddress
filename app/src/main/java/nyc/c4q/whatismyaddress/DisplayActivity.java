2ce2ffeb57c1af55cc368df3e513f2ce531dd922
Yoki Lam <yokeyenlam@ac.c4q.nyc>
2017-11-21 21:57:40 -0500

    part03 commit

--- app/src/main/java/nyc/c4q/whatismyaddress/DisplayActivity.java
+++ app/src/main/java/nyc/c4q/whatismyaddress/DisplayActivity.java
@@ -1,8 +1,26 @@
 package nyc.c4q.whatismyaddress;
 
-/**
- * Created by yokilam on 11/21/17.
- */
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
 
-class DisplayActivity {
+import org.w3c.dom.Text;
+
+public class DisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display2);
+
+        TextView textView01 = (TextView) findViewById(R.id.text1);
+        TextView textView02 = (TextView) findViewById(R.id.text2);
+
+        Intent intent= getIntent();
+        String getString= intent.getStringExtras("email");
+        textView01.setText(getString);
+
+
+    }
 }
